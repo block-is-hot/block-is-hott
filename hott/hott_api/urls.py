5d2a4ea70ef67d598b237f0ecf62da9b0ca01fce
Shannon Tully <scatman.st@gmail.com>
2018-05-22 09:32:57 -0700

    crime api

--- hott/hott_api/urls.py
+++ hott/hott_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserApi
+from .views import UserApi, CrimeMap
 from rest_framework.authtoken import views
 
 urlpatterns = [
     path('user/', UserApi.as_view(), name="user-detail"),
-    path('login/', views.obtain_auth_token)
+    path('login/', views.obtain_auth_token),
+    path('crime/', CrimeMap.as_view(), name='crime'),
 ]
--- hott/hott_api/views.py
+++ hott/hott_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.authentication import TokenAuthentication
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import generics, status
+from rest_framework.views import APIView
+from ipywidgets.embed import embed_minimal_html
+from hott_overlays.models import Crimes
+import gmaps
+import os
 
 
 class UserApi(generics.RetrieveAPIView, generics.CreateAPIView):
@@ -21,4 +25,33 @@ class UserApi(generics.RetrieveAPIView, generics.CreateAPIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CrimeMap(APIView):
+    """Crime map view that takes in our crime data and serves the response."""
+
+    authentication_classes = ''
+    permission_classes = ''
+
+    def get(self, request, format=None):
+        """Get route for crime map."""
+        gmaps.configure(api_key=os.environ.get('MAPS_API'))
+
+        locations = []
+        for each in Crimes.objects.all():
+            temp = []
+            temp.append(each.latitude)
+            temp.append(each.longitude)
+            locations.append(temp)
+
+        heatmap_layer = gmaps.heatmap_layer(locations)
+
+        fig = gmaps.figure()
+
+        fig.add_layer(heatmap_layer)
+        embed_minimal_html('export.html', views=[fig])
+
+        export = open('export.html').read()
+
+        return Response(export)
